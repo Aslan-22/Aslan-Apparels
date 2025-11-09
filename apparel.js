027962759d7a6276891a76b40258bb40d40e9be0
Aslan-22 <147937357+Aslan-22@users.noreply.github.com>
2025-11-09 19:13:09 +0300

    Initial commit for Aslan Apparels

---
+++ apparel.js
@@ -0,0 +1,22 @@
+// Testimonial slider
+let currentSlide = 0;
+const  slides = document.querySelectorAll('.testimonial-item');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1)%slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Initialize the slider
+showSlide(currentSlide);
